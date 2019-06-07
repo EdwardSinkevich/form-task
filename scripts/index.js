13d0b811069d946bf62790d8aae17c45ef471571
Edvard Sinkevich <mejik-yo@mail.ru>
2019-06-08 02:40:00 +0300

    feat: "add validate to some fields"

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,12 @@
 const dayField = document.getElementById("day-field");
 const yearField = document.getElementById("year-field");
 const graduationField = document.getElementById("graduation-field");
+const phoneField = document.getElementById("phone-field");
+
+const wrongDateBirth = document.getElementById("wrong-birth-input");
+const wrongGraduationInput = document.getElementById("wrong-graduation-input");
+
+let validateTtrigger = true;
 
 function filterDate(e) {
   let theEvent = e || window.event;
@@ -23,20 +29,66 @@ dayField.addEventListener("keypress", filterDate);
 yearField.addEventListener("keypress", filterDate);
 graduationField.addEventListener("keypress", filterDate);
 
+//validate birthday
+function birthdayValidate() {
+  if (dayField.value <= 0 || dayField.value >= 32
+    || yearField.value <= 1960 || yearField.value >= 2006) {
+    wrongDateBirth.style.display = "inline-block";
+    validateTtrigger = false;
+  } else {
+    wrongDateBirth.style.display = "none";
+    validateTtrigger = true;
+  }
+};
+
+dayField.addEventListener('blur', birthdayValidate);
+yearField.addEventListener('blur', birthdayValidate);
+
+//validate graduation field
+function graduationValidate() {
+  if (this.value <= 1960 || this.value >= 2019) {
+    wrongGraduationInput.style.display = "inline-block";
+    validateTtrigger = false;
+  } else {
+    wrongGraduationInput.style.display = "none";
+    validateTtrigger = true;
+  }
+};
+graduationField.addEventListener('blur', graduationValidate);
+
+//phone mask
+function phoneMask(e) {
+  const x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,2})(\d{0,2})(\d{0,2})(\d{0,3})/);
+  e.target.value = !x[2] ? '+375-' : '+375-' + (!x[3] ? x[2] : '(' + x[2] + ') ') + x[3] + (x[4] ? '-' + x[4] : '') + (x[5] ? '-' + x[5] : '');
+};
+
+function changePhoneColor() {
+  if (this.value.length <= 5) {
+    this.classList.add('phone-field');
+  } else {
+    this.classList.remove('phone-field');
+  }
+}
+
+phoneField.addEventListener('input', phoneMask);
+phoneField.addEventListener('input', changePhoneColor);
+
 // submitting form
 let questionnaireForm = document.getElementById("questionnaire-form");
 const formData = {};
 
 function getFormData() {
-  questionnaireForm = document.getElementById("questionnaire-form");
-  const questionnaireFormData = new FormData(questionnaireForm);
-  const resultForm = document.getElementById("result-form");
+  if(validateTtrigger) {
+    questionnaireForm = document.getElementById("questionnaire-form");
+    const questionnaireFormData = new FormData(questionnaireForm);
+    const resultForm = document.getElementById("result-form");
 
-  for (let [key, value] of questionnaireFormData) {
-    formData[key] = value;
+    for (let [key, value] of questionnaireFormData) {
+      formData[key] = value;
+    }
+    insertData(formData);
+    resultForm.style.display = "flex";
   }
-  insertData(formData);
-  resultForm.style.display = "flex";
 }
 
 function insertData(data) {
